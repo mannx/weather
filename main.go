5723d5725d02e2e9e5f8d16cfd6399295447569b
none <none@invalid.com>
2021-12-26 17:02:10 -0400

    initial echo setup done

--- main.go
+++ main.go
@@ -1,150 +1,43 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"io"
 	"log"
-	"text/template"
 
-	"encoding/json"
+	"time"
+
+	"html/template"
 	"net/http"
 
+	"database/sql"
+
 	_ "modernc.org/sqlite"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 const cityID = 6138517
 const apiKey = "8500043bc3c464bdc0a90c69333c50b9"
 
-// weatherToMap converts a JSON type mapping into a singular flat map with
-//  sub levels starting with the tld string
-//	ie.
-//		"weather":
-//			"code": 102
-//	converts to map["weather.code"]=102
-//
-func weatherToMap(data map[string]interface{}, output *map[string]interface{}, tld string) {
-	for k, v := range data {
-		switch n := v.(type) {
-		case map[string]interface{}: // data deeper in
-			t := fmt.Sprintf("%s.%s", tld, k)
-			weatherToMap(n, output, t)
-		case []interface{}:
-			// array of map[]'s
-			for _, x := range n {
-				switch m := x.(type) {
-				case map[string]interface{}:
-					t := fmt.Sprintf("%s.%s", tld, k)
-					weatherToMap(m, output, t)
-				}
-			}
-		default:
-			// get the name of this item using the tld, and current key
-			// and store the value
-			t := fmt.Sprintf("%s.%s", tld, k)
-			(*output)[t] = v
-		}
-	}
+// Template for doing template things
+type Template struct {
+	templates *template.Template
 }
 
-func getFloat64(in map[string]interface{}, key string) float64 {
-	v := in[key]
-	if v == nil {
-		return 0.0
-	}
-	return v.(float64)
+// Render for template rendering things
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-func getWeatherData(input map[string]interface{}) WeatherData {
-	wd := WeatherData{}
-
-	// copy over the float64's values
-	wd.code = input[".weather.id"].(float64)
-	wd.Temp = input[".main.temp"].(float64)
-	wd.FeelsLike = input[".main.feels_like"].(float64)
-	wd.Pressure = input[".main.pressure"].(float64)
-	wd.Humidity = input[".main.humidity"].(float64)
-	wd.WindSpeed = input[".wind.speed"].(float64)
-	wd.WindDir = input[".wind.deg"].(float64)
-	wd.WindGust = getFloat64(input, ".wind.gust")
-	wd.Rain1h = getFloat64(input, ".rain.rain.1h")
-	wd.Rain3h = getFloat64(input, ".rain.rain.3h")
-	wd.Snow1h = getFloat64(input, ".snow.snow.1h")
-	wd.Snow3h = getFloat64(input, ".snow.snow.3h")
-
-	wd.Icon = input[".weather.icon"].(string)
-	wd.sunrise = getFloat64(input, ".sys.sunrise")
-	wd.sunset = getFloat64(input, ".sys.runset")
-	wd.name = input[".name"].(string)
-
-	wd.time = getFloat64(input, ".dt")
-
-	return wd
-}
-
-func getCurrentWeather() {
-	// retrieve the current weather in json format
-	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?id=%v&appid=%v&units=metric", cityID, apiKey)
-	resp, err := http.Get(url)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var res map[string]interface{}
-	json.Unmarshal([]byte(body), &res)
-
-	output := make(map[string]interface{})
-	weatherToMap(res, &output, "")
-
-	wd := getWeatherData(output)
-
-	// open the db
-	db, err := sql.Open("sqlite", "./db.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
-
-	sql := "CREATE TABLE IF NOT EXISTS weather (id INTEGER PRIMARY KEY, time TEXT, code REAL, temp REAL, feelsLike REAL, pressure REAL, humidity REAL, windSpeed REAL, windDir REAL, "
-	sql = fmt.Sprintf("%swindGust REAL, rain1h REAL, rain3h REAL, snow1h REAL, snow3h REAL, icon TEXT, city INTEGER)", sql)
-
-	state, err := db.Prepare(sql)
-	if err != nil {
-		log.Fatal(err)
-	}
-	state.Exec()
-
-	// # of values to insert: 13
-	sql = "INSERT INTO weather (time, code, temp, feelsLike, pressure, humidity, windSpeed, windDir, windGust, rain1h, rain3h, snow1h, snow3h, icon, city) VALUES (?,?,?, ?,?,?, ?,?,?, ?,?,?, ?,?,?)"
-	stmt, err := db.Prepare(sql)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer stmt.Close() // make sure to free resources
-
-	_, err = stmt.Exec(wd.time, wd.code, wd.Temp, wd.FeelsLike, wd.Pressure, wd.Humidity, wd.WindSpeed, wd.WindDir, wd.WindGust, wd.Rain1h, wd.Rain3h, wd.Snow1h, wd.Snow3h, wd.Icon, cityID)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//fmt.Println("Current weather logged sucessfully!")
-}
-
-func newWeatherHandler(w http.ResponseWriter, r *http.Request) {
+//func newWeatherHandler(w http.ResponseWriter, r *http.Request) {
+func newWeatherHandler(c echo.Context) error {
 	getCurrentWeather()
-	fmt.Fprintf(w, "Current weather logged successfully!")
+	//	fmt.Fprintf(w, "Current weather logged successfully!")
+	return c.String(http.StatusOK, "Weather Updated Successfully!")
 }
 
-func viewWeatherHandler(w http.ResponseWriter, req *http.Request) {
+func viewWeatherHandler(c echo.Context) error {
 	// open the db and show the latest weather report
 	db, err := sql.Open("sqlite", "db.db")
 	if err != nil {
@@ -155,20 +48,30 @@ func viewWeatherHandler(w http.ResponseWriter, req *http.Request) {
 
 	wd := GetWeatherData(db)
 
-	t, err := template.ParseFiles("latest.html")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// convert the time into a user friendly time string
+	t := time.Unix(int64(wd.Time), 0)
+	wd.TimeString = t.String()
 
-	err = t.Execute(w, &wd)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return c.Render(http.StatusOK, "temp.html", wd)
 }
 
 func main() {
-	http.HandleFunc("/new", newWeatherHandler)
-	http.HandleFunc("/view", viewWeatherHandler)
+	t := &Template{
+		templates: template.Must(template.ParseGlob("./static/*.html")),
+	}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	e := echo.New()
+
+	// middle ware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Renderer = t
+
+	// routes
+	e.GET("/view", viewWeatherHandler)
+	e.GET("/new", newWeatherHandler)
+
+	// start the server
+	e.Logger.Fatal(e.Start(":8080"))
 }
--- types.go
+++ types.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 )
 
+// WeatherData structure holds data for a given weather pull
 type WeatherData struct {
-	time      float64
+	Time      float64
+	TimeZone  float64
 	code      float64
 	Temp      float64
 	FeelsLike float64
@@ -24,10 +30,13 @@ type WeatherData struct {
 	sunrise float64
 	sunset  float64
 	name    string // name of the city we are storing data for
+
+	TimeString string // used only before send off to the template engine to display a user friendly Time string
 }
 
+// GetWeatherData builds a WeatherData object from the database using the last entry
 func GetWeatherData(db *sql.DB) WeatherData {
-	s := "SELECT temp, feelsLike, humidity, windSpeed, windDir, rain1h, snow1h, icon FROM weather ORDER BY id DESC LIMIT 1"
+	s := "SELECT time, Timezone, temp, feelsLike, humidity, windSpeed, windDir, rain1h, snow1h, icon FROM weather ORDER BY id DESC LIMIT 1"
 	r, err := db.Query(s)
 	if err != nil {
 		log.Fatal(err)
@@ -38,10 +47,134 @@ func GetWeatherData(db *sql.DB) WeatherData {
 	wd := WeatherData{}
 
 	for r.Next() {
-		if err := r.Scan(&wd.Temp, &wd.FeelsLike, &wd.Humidity, &wd.WindSpeed, &wd.WindDir, &wd.Rain1h, &wd.Snow1h, &wd.Icon); err != nil {
+		if err := r.Scan(&wd.Time, &wd.TimeZone, &wd.Temp, &wd.FeelsLike, &wd.Humidity, &wd.WindSpeed, &wd.WindDir, &wd.Rain1h, &wd.Snow1h, &wd.Icon); err != nil {
 			log.Fatal(err)
 		}
 	}
 
 	return wd
+}
+
+// weatherToMap converts a JSON type mapping into a singular flat map with
+//  sub levels starting with the tld string
+//	ie.
+//		"weather":
+//			"code": 102
+//	converts to map["weather.code"]=102
+//
+func weatherToMap(data map[string]interface{}, output *map[string]interface{}, tld string) {
+	for k, v := range data {
+		switch n := v.(type) {
+		case map[string]interface{}: // data deeper in
+			t := fmt.Sprintf("%s.%s", tld, k)
+			weatherToMap(n, output, t)
+		case []interface{}:
+			// array of map[]'s
+			for _, x := range n {
+				switch m := x.(type) {
+				case map[string]interface{}:
+					t := fmt.Sprintf("%s.%s", tld, k)
+					weatherToMap(m, output, t)
+				}
+			}
+		default:
+			// get the name of this item using the tld, and current key
+			// and store the value
+			t := fmt.Sprintf("%s.%s", tld, k)
+			(*output)[t] = v
+		}
+	}
+}
+
+func getFloat64(in map[string]interface{}, key string) float64 {
+	v := in[key]
+	if v == nil {
+		return 0.0
+	}
+	return v.(float64)
+}
+
+func getWeatherData(input map[string]interface{}) WeatherData {
+	wd := WeatherData{}
+
+	// copy over the float64's values
+	wd.code = input[".weather.id"].(float64)
+	wd.Temp = input[".main.temp"].(float64)
+	wd.FeelsLike = input[".main.feels_like"].(float64)
+	wd.Pressure = input[".main.pressure"].(float64)
+	wd.Humidity = input[".main.humidity"].(float64)
+	wd.WindSpeed = input[".wind.speed"].(float64)
+	wd.WindDir = input[".wind.deg"].(float64)
+	wd.WindGust = getFloat64(input, ".wind.gust")
+	wd.Rain1h = getFloat64(input, ".rain.rain.1h")
+	wd.Rain3h = getFloat64(input, ".rain.rain.3h")
+	wd.Snow1h = getFloat64(input, ".snow.snow.1h")
+	wd.Snow3h = getFloat64(input, ".snow.snow.3h")
+
+	wd.Icon = input[".weather.icon"].(string)
+	wd.sunrise = getFloat64(input, ".sys.sunrise")
+	wd.sunset = getFloat64(input, ".sys.runset")
+	wd.name = input[".name"].(string)
+
+	wd.Time = getFloat64(input, ".dt")
+	wd.TimeZone = getFloat64(input, ".Timezone")
+
+	return wd
+}
+
+func getCurrentWeather() {
+	// retrieve the current weather in json format
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?id=%v&appid=%v&units=metric", cityID, apiKey)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var res map[string]interface{}
+	json.Unmarshal([]byte(body), &res)
+
+	output := make(map[string]interface{})
+	weatherToMap(res, &output, "")
+
+	wd := getWeatherData(output)
+
+	// open the db
+	db, err := sql.Open("sqlite", "./db.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	sql := "CREATE TABLE IF NOT EXISTS weather (id INTEGER PRIMARY KEY, Time REAL, code REAL, temp REAL, feelsLike REAL, pressure REAL, humidity REAL, windSpeed REAL, windDir REAL, "
+	sql = fmt.Sprintf("%swindGust REAL, rain1h REAL, rain3h REAL, snow1h REAL, snow3h REAL, icon TEXT, city INTEGER, Timezone REAL)", sql)
+
+	state, err := db.Prepare(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	state.Exec()
+
+	// # of values to insert: 13
+	sql = "INSERT INTO weather (Time, code, temp, feelsLike, pressure, humidity, windSpeed, windDir, windGust, rain1h, rain3h, snow1h, snow3h, icon, city, Timezone) VALUES (?,?,?, ?,?,?, ?,?,?, ?,?,?, ?,?,?, ?)"
+	stmt, err := db.Prepare(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close() // make sure to free resources
+
+	_, err = stmt.Exec(wd.Time, wd.code, wd.Temp, wd.FeelsLike, wd.Pressure, wd.Humidity, wd.WindSpeed, wd.WindDir, wd.WindGust, wd.Rain1h, wd.Rain3h, wd.Snow1h, wd.Snow3h, wd.Icon, cityID, wd.TimeZone)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Println("Current weather logged sucessfully!")
 }

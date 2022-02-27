c393bf64e0ecddc214266647bf35ceca43d5f197
mint <18175956+mannx@users.noreply.github.com>
2022-02-27 14:28:12 -0400

    code cleanup

--- server.go
+++ server.go
@@ -21,10 +21,6 @@ func initServer() *echo.Echo {
 	e.Use(middleware.Static("./static"))
 
 	// routes
-	/*e.GET("/api/24hr", handle24hrView)
-	e.GET("/api/latest", getLatestWeatherView)
-	e.GET("/api/daily", getDailyWeatherView)*/
-
 	e.GET("/api/24hr", func(c echo.Context) error { return api.Handle24hrView(c, DB) })
 	e.GET("/api/latest", func(c echo.Context) error { return api.GetLatestWeatherView(c, DB) })
 	e.GET("api/daily", func(c echo.Context) error { return api.GetDailyWeatherView(c, DB) })
